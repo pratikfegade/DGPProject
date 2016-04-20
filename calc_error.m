af456cd48dc5e9c155f5e6fb17a45fda6ed91fed
Siddhant Ranade <siddhant.ranade@gmail.com>
2016-04-20 17:45:40 +0530

    Sorta fixed smoothness penalty, added collision penalty, combined gap penalty and collision penalty into a single file.

--- calc_error.m
+++ calc_error.m
@@ -5,8 +5,10 @@ function [error] = calc_error(surface, skeleton)
 
     error = 0;
     
-    error = error + gap_penalty_multiplier * gap_penalty(surface, skeleton);
-    error = error + collision_penalty_multiplier * collision_penalty(surface, skeleton);
+    [gap_penalty,collision_penalty] = gap_collision_penalty(surface, skeleton);
+    
+    error = error + gap_penalty_multiplier * gap_penalty;
+    error = error + collision_penalty_multiplier * collision_penalty;
     error = error + smoothness_penalty_multiplier * smoothness_penalty(surface, skeleton);
     error = error + concavity_penalty_multiplier * concavity_penalty(surface, skeleton);
     error = error + prior_penalty_multiplier * prior_penalty(surface, skeleton);
--- smoothness_penalty.m
+++ smoothness_penalty.m
@@ -2,7 +2,6 @@ function [penalty] = smoothness_penalty(surface, skeleton)
     global grid_resolution;
     padded_surface = padarray(surface, [2,2], 'replicate', 'both');
     padded_surface = padarray(padded_surface, [2,1], 'replicate', 'both');
-    vert_c = conv2(padded_surface, [-1,0,1;-2,0,2;-1,0,1]);
-    horz_c = conv2(padded_surface, [-1, -2, -1; 0,0,0; 1,2,1]);
-    penalty = sum(sum(abs(vert_c(4:2*grid_resolution+4, 4:2*grid_resolution+4,1)))) + sum(sum(abs(norm(horz_c(4:2*grid_resolution+4, 4:2*grid_resolution+4),1))));
+    lapl = conv2(padded_surface, [0,1,0;1,-4,1;0,1,0]);
+    penalty = sum(sum(abs(lapl(4:2*grid_resolution+4, 4:2*grid_resolution+4,1))));
 end
